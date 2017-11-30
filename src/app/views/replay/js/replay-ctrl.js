3b324fa69be279bf72d0e3aeef6da64eae274140
wangmeng09 <18716003620@163.com>
2017-11-30 09:36:31 +0800

    增加模块

---
+++ src/app/views/replay/js/replay-ctrl.js
@@ -0,0 +1,7 @@
+angular.module("myApp").controller("ReplayListCtrl",["$scope","ReplayService",function ($scope,ReplayService) {
+
+}]);
+
+myApp.controller("ReplayDetailCtrl",["$scope","$rootScope",'$stateParams','ReplayService',function ($scope,$rootScope,$stateParams,ReplayService) {
+
+}]);
