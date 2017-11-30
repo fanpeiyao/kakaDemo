4a49d9e2948aad824af8436d5d736b1eb52c8928
wangmeng09 <18716003620@163.com>
2017-11-30 15:22:09 +0800

    加弹框提示

--- src/app/views/repayment/js/repayment-ctrl.js
+++ src/app/views/repayment/js/repayment-ctrl.js
@@ -2,6 +2,8 @@
  * 还款
  */
 angular.module("myApp").controller("RepaymentCtrl",["$scope","RepaymentService",function ($scope,RepaymentService) {
-    $
+    $scope.repayment=function(){
+        $.toast.content("还款成功").show('info',2000).clear();
+    }
 
 }]);
