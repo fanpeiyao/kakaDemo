2965a1b72ec5f35ca779b267137674c1f7534960
wangmeng09 <18716003620@163.com>
2017-11-30 14:36:56 +0800

    关于到账明细

--- src/app/views/replay/js/replay-ctrl.js
+++ src/app/views/replay/js/replay-ctrl.js
@@ -1,5 +1,101 @@
 angular.module("myApp").controller("ReplayListCtrl",["$scope","ReplayService",function ($scope,ReplayService) {
+    var page,time,status;
 
+    $scope.changeStatus = function(newStatus){
+        page = 0;
+        time = 0;
+        status = newStatus;
+        $scope.deliverys = loadDeliverys(status,page,time);
+    };
+    $scope.changeTime = function(newTime){
+        page = 0;
+        time = newTime;
+        $scope.deliverys= loadDeliverys(status,page,newTime);
+    };
+
+    $scope.refreshPage = function () {
+        $scope.deliverys = loadDeliverys(status,page,time);
+    };
+
+    $scope.loadMore = function () {
+        page = page+1;
+        var deliverys = loadDeliverys(status,page,time);
+        if(deliverys.length>0)
+            for(var i=0,len=deliverys.length; i<len; i++){
+                $scope.deliverys.push(deliverys[i]);
+            }
+    };
+
+    //function loadDeliverys(status,page,time) {
+    //    return DeliveryService.getDeliveryList(status,page,time);
+    //}
+    function loadDeliverys(status,page,time) {
+        var toast = $.toast.show('loading');
+        var result = ReplayService.getDeliveryList(status,page,time);
+        if(toast)
+            setTimeout(function(){
+                toast.close();
+            },0);
+        if(result.retCode == "200"){
+            return result.deliveryList;
+        }else
+            return [];
+    }
+    function initPage(){
+        page =0;time=0;
+        var deliveryStatusArray = ReplayService.getDeliveryStatusArray();
+        $scope.deliveryStatusArray = deliveryStatusArray;
+        status = deliveryStatusArray[0];
+        $scope.deliverys = loadDeliverys(status,page,time);
+    }
+
+
+
+
+    //搜索
+    $scope.showSearch = function () {
+        $scope.showSearchBody = true;
+    }
+    $scope.closeSearch = function () {
+        $scope.showSearchBody = false;
+    }
+    $scope.keySearch = function (keyValue) {
+        $scope.searchVal = keyValue;
+        $scope.page = 0;
+        loadOrders();
+        $scope.showSearchBody = false;
+        saveSearchHistory(keyValue);
+    }
+    $scope.clickClean = function ($event) {
+        $event.stopPropagation();
+        $scope.searchVal = '';
+        $scope.page = 0;
+        if(!$scope.showSearchBody)
+            loadOrders()
+    }
+    function saveSearchHistory(keyValue) {
+        if (keyValue === null || keyValue ==="")
+            return
+        var hisstring = istore.getLocal('contractHistory') || [];
+        var dup = false;
+        hisstring.forEach(function (item) {
+            if (item == keyValue)
+                dup = true;
+        })
+        if (!dup){
+            if (hisstring.length<10){
+                hisstring.unshift(keyValue)
+            }else {
+                hisstring.pop();
+                hisstring.unshift(keyValue)
+            }
+            istore.setLocal('contractHistory',hisstring)
+        }
+    }
+
+
+    //初始化
+    initPage();
 }]);
 
 
